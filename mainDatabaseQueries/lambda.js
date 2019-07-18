5035f343ed7ef4e2ffa0286f66df83172295fbdd
ghariosk <karlgharios@hotmail.com>
2019-07-18 17:20:53 +0300

    cleaning up

--- mainDatabaseQueries/lambda.js
+++ mainDatabaseQueries/lambda.js
@@ -3,49 +3,39 @@ let connectionManager = require('./ConnectionManager2');
 const rds = new SL_AWS.RDS(connectionManager);
 
 exports.handler = async function (event, context, callback) {
-
     // You must always end/destroy the DB connection after it's used
-
-    // You can pass the existing connection to this function.
-    // A new connection will be created if it's not present as the third param 
-    // You must always end/destroy the DB connection after it's used
-
-    let Test = async () => { 
+    let Query = async () => { 
         return new Promise((resolve,reject) => {
             rds.query({
-            instanceIdentifier: 'Main',
-            query: 'SELECT * FROM users;',
-            inserts: [0]
-            }, function (error, results, connection) {
-                if (error) {
-                    console.log("Error occurred");
-                    throw error;
-                } else {
-                    console.log("Success")
-                    
-                    
-                
+                instanceIdentifier: 'Main',
+                query: 'SELECT * FROM users;',
+                inserts: [0]
+                }, function (error, results, connection) {
+                    if (error) {
+                        console.log("Error occurred");
+                        throw error;
+                    } else {
+                        console.log("Success")
+                    }
+                    connection.end();
+                    if (results) {
+                        resolve(results)
+                    }
                 }
-
-            connection.end();
-            resolve(results)
+            )
         })
-    })
+    };
+    result = await Query();
+    let headers =  {
+        'content-type': [{
+                key: 'Content-Type',
+                value: 'application/json'
+        }],
+            'content-encoding': [{
+                key: 'Content-Encoding',
+                value: 'UTF-8'
+        }]
     }
-
-
-
-    // callback(null, { "message": "Successfully executed" });
-
-    // let response = await {
-    //     statusCode: "200",
-    //     headers: {
-    //         "x-custom-header" : "my custom header value"
-    //     },
-    //     body: JSON.stringify(Test)
-    // };
-    result = await Test();
-
     console.log("response: " + JSON.stringify(result))
-    return callback(null, {body: JSON.stringify(result), header: "200"});
+    return callback(null, {body: result, status: "200", headers: headers });
 }
